7eda34c369b5cffa3e1a4b7f203e315edf60f962
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-12 21:05:47 -0700

    [TEST] Add test for default and custom callbacks.

--- test/dogecoin_listener.js
+++ test/dogecoin_listener.js
@@ -1,17 +1,86 @@
-var DogecoinListener = require('../lib/dogecoin_listener');
+"use strict";
+
+const DogecoinListener = require('../lib/dogecoin_listener');
+const assert = require('assert');
+const sinon = require('sinon');
+
+const defaultParameters = {
+  dogecoinRpcUrl: 'https://localhost:5533',
+  dogecoinRpcUsername: 'somegreatusername',
+  dogecoinRpcPassword: 'asupersecurep@assword'
+};
 
 describe('dogecoin listener', function() {
+  before(function() {
 
-  before(function(){
 
-    listener = new DogecoinListener();
+  });
+
+  it('should have default behavior for incoming payments', function(fn){
+
+    let defaultListener = new DogecoinListener(defaultParameters);
+
+    let log = console.log;
+    console.log = sinon.spy();
+
+    let payment = {
+      amount: 10,
+      address: 'someDogecoinAddress',
+      hash: 'theDogecoinPaymentHash'
+    };
+
+    defaultListener._incomingPayment(payment, function(err, response) {
+  
+      assert(console.log.calledWith(payment));
+      assert(!err);
+      assert(response == 'incoming dogecoin payment logged to stdout');
+      console.log = log;
+      fn();
+
+    });
+
+  });
+
+  it('should accept a custom callback function', function(fn) {
+    let log = console.log;
+    console.log = sinon.spy();
+
+    let customListenerParams = new Object(defaultParameters);
+
+    customListenerParams.onIncomingPayment = function(payment, fn){
+      log('on incoming payment custom');
+
+      console.log({
+        amount: payment.amount / 2,
+        address: payment.address,
+        hash: payment.hash 
+      });
+  
+      fn(null, true);
+
+    };
     
-  }); 
+    let customListener = new DogecoinListener(customListenerParams);
 
-  it('should throw an error without required options', function(fn) {
-    
-    fn();
+    let payment = {
+      amount: 10,
+      address: 'someDogecoinAddress',
+      hash: 'theDogecoinPaymentHash'
+    };
 
+    customListener._incomingPayment(payment, function(err, response) {
+  
+      assert(console.log.calledWith({
+        amount: 5,
+        address: 'someDogecoinAddress',
+        hash: 'theDogecoinPaymentHash'
+      }));
+
+      console.log = log;
+      fn();
+
+    });
+ 
   });
 
 });
